600359d3aee4a87c95073a014665b1f972906bda
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-03-28 05:41:51 -0700

    add list all users

--- handle_user.go
+++ handle_user.go
@@ -53,6 +53,23 @@ func handlerLogin(s *state, cmd command) error {
 	return nil
 }
 
+func handlerUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't list users: %w", err)
+	}
+
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf(" * %s (current)\n", user.Name)
+			continue
+		}
+		fmt.Printf(" * %s\n", user.Name)
+	}
+
+	return nil
+}
+
 func printUser(user database.User) {
 	fmt.Printf(" * ID:      %v\n", user.ID)
 	fmt.Printf(" * Name:    %v\n", user.Name)
